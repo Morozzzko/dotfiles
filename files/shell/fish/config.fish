d6a903be23f94e128828f2b09b457956e7d63123
Igor S. Morozov <i.morozov@bookmate.com>
2019-11-18 15:48:17 +0300

    Add asdf & ruby-related abbreviations

--- files/shell/fish/config.fish
+++ files/shell/fish/config.fish
@@ -2,3 +2,11 @@ source ~/.profile
 
 abbr -a -g -- gst 'tig status'
 abbr -a -U -- gst 'tig status'
+
+abbr -a be bundle exec
+abbr -a rails bundle exec rails
+abbr -a rspec bundle exec rspec
+abbr -a rake bundle exec rake
+abbr -a reek bundle exec reek
+
+source /usr/local/opt/asdf/asdf.fish
