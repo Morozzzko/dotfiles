2c739018a33a5b3e4b8d25b1444068063554d0aa
Igor S. Morozov <igor@morozov.is>
2020-11-26 17:09:01 +0300

    Add more aliases

--- files/shell/fish/config.fish
+++ files/shell/fish/config.fish
@@ -8,5 +8,14 @@ abbr -a rails bundle exec rails
 abbr -a rspec bundle exec rspec
 abbr -a rake bundle exec rake
 abbr -a reek bundle exec reek
+abbr -a jekyll bundle exec jekyll
+abbr -a rubocop bundle exec rubocop
+abbr -a gpf git push --force-with-lease
+abbr -a grs git restore --source master
+abbr -a gpo git push -u origin (git branch --show-current)
+abbr -a gbm git branch -m
+
+abbr gbda 'git branch --no-color --merged | command grep -vE "^(\+|\*|\s*(master|develop|dev)\s*\$)" | command xargs -n 1 git branch -d'
 
 source /usr/local/opt/asdf/asdf.fish
+set -g fish_user_paths "/usr/local/opt/libpq/bin" $fish_user_paths
