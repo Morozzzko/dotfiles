ce60f0be2fa366b12b42c17758fc216a223dc27e
Igor Morozov <igor@morozov.is>
2026-06-17 00:37:43 +0200

    Update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,6 +10,7 @@ abbr -a rails bin/rails
 abbr -a jekyll bin/jekyll
 abbr -a rubocop bin/rubocop
 abbr -a bridgetown bin/bridgetown
+abbr -a gc git commit
 abbr -a grs git restore --source main
 abbr -a gpo 'git push -u origin (git branch --show-current)'
 abbr -a gpof 'git push -u origin (git branch --show-current) --force-with-lease'
