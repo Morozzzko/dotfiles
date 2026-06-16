fc4bb0b41e5a5a4d9795118e0343e5a1e06922eb
Igor Morozov <igor@morozov.is>
2026-06-16 14:34:58 +0200

    Fix lualine: depend on catppuccin so theme loads before statusline

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -24,7 +24,7 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
     event = "VeryLazy",
     opts = {
       options = {
